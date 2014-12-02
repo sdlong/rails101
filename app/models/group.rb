9e705089b16ad903ea64968e5435a0dedd3b3d5f
sdlong <sdlong.jeng@gmail.com>
2014-12-02 15:28:57 +0800

    setting user and group 多對多資料庫關聯

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,6 +2,9 @@ class Group < ActiveRecord::Base
   validates :title, presence: true
 
   has_many :posts
+  has_many :group_users
+  has_many :memebers, through: :group_users, source: :user
+
   belongs_to :owner, class_name: "User", foreign_key: :user_id
 
   def editable_by?(user)
