fd9cd1778de06d82170169496472e6c681a7a4f7
sdlong <sdlong.jeng@gmail.com>
2014-12-02 12:28:26 +0800

    rake db:migrate migrate_create_post

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141202031318) do
+ActiveRecord::Schema.define(version: 20141202042713) do
 
   create_table "groups", force: true do |t|
     t.string   "title"
     t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "posts", force: true do |t|
+    t.integer  "group_id"
+    t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
