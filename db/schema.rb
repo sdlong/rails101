763c168c45884e742dd3680a7d16aa6f8dcf43d8
sdlong <sdlong.jeng@gmail.com>
2014-12-02 16:54:00 +0800

    rake db:migrate add_user_id_to_post

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141202065045) do
+ActiveRecord::Schema.define(version: 20141202070127) do
 
   create_table "groups", force: true do |t|
     t.string   "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20141202065045) do
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
