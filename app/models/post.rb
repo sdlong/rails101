3142d1eecbd525f5bf28e0755bd10e438c5b4c15
sdlong <sdlong@techbang.com.tw>
2014-12-03 12:19:03 +0800

    使用 scope 整理 query

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
   validates :content, presence: true
+  scope :recent, -> { order("updated_at DESC") }
 
   belongs_to :group, counter_cache: :posts_count
   belongs_to :author, class_name: "User", foreign_key: :user_id
